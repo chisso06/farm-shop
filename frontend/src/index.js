f012dff89a95ae1e3a6032d7f7929f0ecb5a75c5
miyu-wl <tkgth0725@gmail.com>
2024-07-26 06:27:51 +0900

    deploy setting proxy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
-import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_ORIGIN;
+// axios.defaults.baseURL = process.env.REACT_APP_BACKEND_ORIGIN;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
