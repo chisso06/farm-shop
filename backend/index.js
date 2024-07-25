df35435633931164a5bd45cc5d5f3d44ca5e39b0
miyu-wl <tkgth0725@gmail.com>
2024-07-26 07:04:56 +0900

    deploy setting proxy

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
 	if (origin === FRONTEND_ORIGIN) {
 		res.header('Access-Control-Allow-Origin', origin);
 		res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-		res.header('Access-Control-Allow-Methods', 'PUT, DELETE, OPTIONS');
+		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 	}
 	if ('OPTIONS' == req.method) {
     res.send(204);
