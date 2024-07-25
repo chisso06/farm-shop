4a3d7459635d2142483956fb67275f56a44fe712
miyu-wl <tkgth0725@gmail.com>
2024-07-26 06:49:07 +0900

    deploy setting proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,11 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//   app.use(
-//     '/backend',
-//     createProxyMiddleware({
-//       target: process.env.REACT_APP_BACKEND_ORIGIN,
-//       changeOrigin: true,
-//     })
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    '/backend',
+    createProxyMiddleware({
+      target: process.env.REACT_APP_BACKEND_ORIGIN,
+      changeOrigin: true,
+    })
+  );
+};
