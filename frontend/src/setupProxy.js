a97d793c4ea3f31293541d6cb2847a10ca62f75a
miyu-wl <tkgth0725@gmail.com>
2024-07-26 00:26:22 +0900

    deploy setting proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/backend',
+    'https://farm-shop-deploy.vercel.app/backend',
     createProxyMiddleware({
       target: "https://farm-shop-deploy.vercel.app",
       changeOrigin: true,
