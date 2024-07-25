e153466683e3cac95cd57fb53916efb8f61e889f
miyu-wl <tkgth0725@gmail.com>
2024-07-26 08:39:18 +0900

    deploy setting axios

--- backend/index.js
+++ backend/index.js
@@ -64,7 +64,7 @@ app.use((req, res, next) => {
 	// 	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 	// 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 	// }
-	res.setHeader('Access-Control-Allow-Credentials', true)
+	// res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_ORIGIN;
-axios.defaults.headers.post['Access-Control-Allow-Credentials'] = 'true';
+// axios.defaults.headers.post['Access-Control-Allow-Credentials'] = 'true';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.post['Access-Control-Allow-Methods'] = 'GET,OPTIONS,PATCH,DELETE,POST,PUT';
 axios.defaults.headers.post['X-Requested-With'] = 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version';
