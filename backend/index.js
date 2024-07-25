756e4b0a51c5b09c69179ec35f22860027f85853
miyu-wl <tkgth0725@gmail.com>
2024-07-26 06:46:33 +0900

    deploy setting proxy

--- backend/index.js
+++ backend/index.js
@@ -37,16 +37,8 @@ const connection = mysql.createConnection({
 });
 const mysqlPromise = require('mysql2/promise');
 
-// // cors
-// const origin = req.headers.origin;
-// if (origin === FRONTEND_ORIGIN) {
-//   res.header('Access-Control-Allow-Origin', origin);
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-//   // res.header('Access-Control-Allow-Methods', 'PUT, DELETE, OPTIONS');
-// }
-
 // cors
-const cors = require('cors');
+// const cors = require('cors');
 
 // crypto
 const crypto = require('crypto');
@@ -61,10 +53,22 @@ const multer  = require('multer');
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use((req, res, next) => {
 	console.log(`[${req.method}] ${req.url}`);
-	next();
+
+	// cors
+	const origin = req.headers.origin;
+	if (origin === FRONTEND_ORIGIN) {
+		res.header('Access-Control-Allow-Origin', origin);
+		res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+		res.header('Access-Control-Allow-Methods', 'PUT, DELETE, OPTIONS');
+	}
+	if ('OPTIONS' == req.method) {
+    res.send(204);
+  } else {
+    next();
+  }
 });
 app.use(express.urlencoded({extended:true}))
 
